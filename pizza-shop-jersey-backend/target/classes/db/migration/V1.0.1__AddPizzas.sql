ef03d6a6bb6d38defe0b661f88e81d519205ff4b
Ali Asad <a2.khowaja@gmail.com>
2020-07-07 01:15:17 +0500

    target folder deleted

--- pizza-shop-jersey-backend/target/classes/db/migration/V1.0.1__AddPizzas.sql
+++
@@ -1,6 +0,0 @@
-insert into pizza (name, description, price)
-values ('Margherita Pizza', 'Tomato sauce, mozzarella, and oregano', 1200),
-('Marinara Pizza', 'Tomato sauce, garlic and basil', 1200),
-('Quattro Stagioni Pizza', 'Tomato sauce, mozzarella, mushrooms, ham, artichokes, olives, and oregano', 1200),
-('Carbonara Pizza', 'Tomato sauce, mozzarella, parmesan, eggs, and bacon', 1200),
-('Frutti di Mare Pizza', 'Tomato sauce and seafood', 1200);
--- pizza-shop-jersey-backend/target/pizzashop/WEB-INF/classes/db/migration/V1.0.0__Pizza.sql
+++
@@ -1,8 +0,0 @@
-create table pizza(
-    id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    price DOUBLE NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY idx_unique_pizza_name (name)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
